8b59a9e6155ed533fad990b9729b34131ae764c3
胡彬 <hubin@biosan.cn>
2019-04-11 13:49:32 +0800

    增加允许跨域访问配置

--- src/main/java/com/mrbeard/process/config/WebConfig.java
+++ src/main/java/com/mrbeard/process/config/WebConfig.java
@@ -1,7 +1,11 @@
 package com.mrbeard.process.config;
 
 import com.mrbeard.process.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.*;
 
 /**
@@ -52,5 +56,32 @@ public class WebConfig implements WebMvcConfigurer {
 //        registry.addStatusController("/static/404.html",HttpStatus.NOT_FOUND);
 //        registry.addStatusController("/static/nepadmin/views/exception/403.html",HttpStatus.FORBIDDEN);
 //        registry.addStatusController("/static/nepadmin/views/exception/500.html",HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * 解决跨域
+     * @return
+     */
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // 1允许任何域名使用
+        corsConfiguration.addAllowedOrigin("*");
+        // 2允许任何头
+        corsConfiguration.addAllowedHeader("*");
+        // 3允许任何方法（post、get等）
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    /**
+     * 跨域
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // 4
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
     }
 }
