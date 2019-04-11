c61ea6008692f2fa3bc8d80101e4f271880904b5
胡彬 <hubin@biosan.cn>
2019-04-11 13:50:26 +0800

    增加访问url日志

--- src/main/java/com/mrbeard/process/interceptor/LoginInterceptor.java
+++ src/main/java/com/mrbeard/process/interceptor/LoginInterceptor.java
@@ -30,6 +30,7 @@ public class LoginInterceptor implements HandlerInterceptor {
      */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("========================RequestUrl"+request.getRequestURL().toString());
         logger.info("========================LoginInterceptor");
         //从request中获取到usertoken
         String usertoken = request.getParameter("usertoken");
